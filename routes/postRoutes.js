90d622b6cd3a1d68406a4064cf76084e5e4fe677
raniaadouni <rania.adouni@esprit.tn>
2022-03-22 17:20:42 +0100

    Express session authentication redis

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -8,13 +8,13 @@ const router =  express.Router()
 //localhost:3000/
 router
 .route("/")
-.get(postController.getAllPosts)
+.get(protect,postController.getAllPosts)
 .post(protect,postController.createPost)
 
 router
 .route('/:id')
-.get(postController.getOnPost)
-.patch(postController.updatePost)
-.delete(postController.deletePost)
+.get(protect,postController.getOnPost)
+.patch(protect,postController.updatePost)
+.delete(protect,postController.deletePost)
 
 module.exports = router;
